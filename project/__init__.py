8502061b0e0b7702a79063ae58ed7397b08426ba
hizaguru <BlacksGithub1940!>
2022-06-14 18:43:04 +0300

    Refactoring

--- project/__init__.py
+++ project/__init__.py
@@ -4,12 +4,13 @@ from flask_assets import Environment, Bundle
 from dotenv import load_dotenv
 import os
 from flask_wtf.csrf import CSRFProtect
+
+
 # init SQLAlchemy so we can use it later in our models
 
 def create_app():
-    
     app = Flask(__name__, template_folder='templates')
-    
+
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
     load_dotenv()
     CSRFProtect(app)
@@ -23,7 +24,7 @@ def create_app():
     assets.config['PYSCSS_ASSETS_URL'] = assets.url
     assets.config['PYSCSS_ASSETS_ROOT'] = assets.directory
     assets.register('scss_all', scss)
-    
+
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
--- project/main.py
+++ project/main.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, render_template
 
+UPLOAD_FORM = "index.html"
 main = Blueprint('main', __name__)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(UPLOAD_FORM)
 
-@main.route('/profile')
+
+@main.route('/uploader')
 def profile():
     return 'Profile'
